28d199949aec59633f55371b4d183c93829ef18b
João Viana <joaovitor@live.it>
2020-12-09 11:44:08 -0400

    Atualização database
    Incluído conexão com MySQL

---
+++ Db/GenericMySql.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace UtilidadesWFA.Db
+{
+    public class GenericMySql
+    {
+        private string ParametroBanco = "@";
+        private MySql Conexao;
+        public GenericMySql(string pServidor, string pUsuario = "root", string pSenha = null)
+        {
+            Conexao = new MySql(pServidor, pUsuario, pSenha);
+        }
+        public List<T> GetSelect<T>(string pComando, List<DBParametros> pParametros)
+        {
+            try
+            {
+                DataTable dtConsulta = Conexao.ExecuteDataTable(pComando.Replace("[", "").Replace("]", ""), pParametros);
+                return new GenericCommand().GetSelect<T>(dtConsulta);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public T Salvar<T>(T pObjeto, string pDataBase)
+        {
+            try
+            {
+                GenericCommand.Generic<T> Ger = new GenericCommand.Generic<T>(pObjeto, pDataBase + "." + typeof(T).Name + " ", ParametroBanco, "; SELECT LAST_INSERT_ID();");
+                if (Ger.CamposPrimaryKeys.Count > 0 && !Ger.Update)
+                    Ger.CamposPrimaryKeys[0].SetValue(pObjeto, Convert.ToInt32(Conexao.ExecuteScalar(Ger.Query, Ger.Parametros)));
+                else
+                    Conexao.ExecuteNonQuery(Ger.Query, Ger.Parametros);
+                return pObjeto;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public string Deletar<T>(T pObjeto, string pDataBase)
+        {
+            try
+            {
+                GenericCommand.Generic<T> Ger = new GenericCommand.Generic<T>(pObjeto, pDataBase + "." + typeof(T).Name, ParametroBanco);
+                Conexao.ExecuteNonQuery(Ger.Query, Ger.Parametros);
+                return "Deletado com sucesso";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<T> GetItens<T>(List<Parametro> pParametros, string pDataBase, int pTop = 0, string pOrderBy = "")
+        {
+            StringBuilder sbQuery = new StringBuilder();
+            sbQuery.AppendLine("SELECT * ");
+            sbQuery.AppendLine("FROM " + pDataBase + "." + typeof(T).Name);
+            sbQuery.AppendLine("WHERE 1=1");
+
+            List<DBParametros> pmts = null;
+            if (pParametros != null)
+            {
+                pmts = new List<DBParametros>();
+                pParametros.ForEach((parametro) =>
+                {
+                    sbQuery.AppendLine("AND (" + parametro.GetCondicao(ParametroBanco, TpBanco.MySql) + ")");
+                    pmts.AddRange(parametro.GetParametros());
+                });
+            }
+
+            if (!string.IsNullOrWhiteSpace(pOrderBy))
+                sbQuery.AppendLine("ORDER BY " + pOrderBy);
+            if (pTop > 0)
+                sbQuery.AppendLine("LIMIT " + pTop.ToString());
+
+            return GetSelect<T>(sbQuery.ToString(), pmts);
+        }
+        public T GetItem<T>(List<Parametro> pParametros, string pDataBase)
+        {
+            try
+            {
+                List<T> lista = GetItens<T>(pParametros, pDataBase);
+                return (lista.Count > 0) ? lista[0] : Activator.CreateInstance<T>();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Db/MySql.cs
@@ -0,0 +1,190 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace UtilidadesWFA.Db
+{
+    public class MySql
+    {
+        protected MySqlConnection Conn;
+        protected MySqlCommand Cmd;
+        protected MySqlTransaction Tra = null;
+
+        private MySqlConnection RetornaConexao(string pConexao, string pUsuario, string pSenha)
+        {
+            MySqlConnectionStringBuilder Connection = new MySqlConnectionStringBuilder();
+            Connection.Server = pConexao;
+            Connection.UserID = pUsuario;
+            if (!string.IsNullOrWhiteSpace(pSenha))
+                Connection.Password = pSenha;
+
+            MySqlConnection oSqlConn = new MySqlConnection();
+            oSqlConn.ConnectionString = Connection.ConnectionString;
+            return oSqlConn;
+        }
+
+        public MySql(string pServer, string pUsuario = "root", string pSenha = null)
+        {
+            try
+            {
+                Conn = new MySqlConnection();
+                Conn = RetornaConexao(pServer, pUsuario, pSenha);
+                Cmd = new MySqlCommand("", Conn);
+                Cmd.CommandTimeout = 100000;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void AbreConexao()
+        {
+            if (Conn.State != ConnectionState.Open)
+                Conn.Open();
+        }
+
+        public void FechaConexao()
+        {
+            if (Conn.State == ConnectionState.Open)
+                Conn.Close();
+        }
+
+        public DataSet ExecuteDataSet(string pComandoSQL, List<DBParametros> pParametros = null)
+        {
+            DataSet ds = new DataSet();
+            AbreConexao();
+
+            try
+            {
+                Cmd.Connection = Conn;
+                Cmd.Parameters.Clear();
+                Cmd.CommandText = pComandoSQL;
+                Cmd.CommandType = CommandType.Text;
+
+                if (pParametros != null)
+                {
+                    foreach (DBParametros item in pParametros)
+                    {
+                        Cmd.Parameters.AddWithValue(item.Name, item.Value == null ? DBNull.Value : item.Value);
+                    }
+                }
+
+                MySqlDataAdapter adp = new MySqlDataAdapter(Cmd);
+                adp.SelectCommand.CommandTimeout = 100000;
+                adp.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                FechaConexao();
+            }
+
+            return ds;
+        }
+
+        public DataTable ExecuteDataTable(string pComandoSQL, List<DBParametros> pParametros = null)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                dt = ExecuteDataSet(pComandoSQL, pParametros).Tables[0];
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return dt;
+        }
+
+        public int ExecuteNonQuery(string pComandoSQL, List<DBParametros> pParametros = null)
+        {
+            int retorno;
+            AbreConexao();
+            try
+            {
+                Cmd.Connection = Conn;
+                Cmd.Parameters.Clear();
+                Cmd.CommandText = pComandoSQL;
+                Cmd.CommandType = CommandType.Text;
+
+                if (pParametros != null)
+                {
+                    foreach (DBParametros item in pParametros)
+                    {
+                        Cmd.Parameters.AddWithValue(item.Name, item.Value == null ? DBNull.Value : item.Value);
+                    }
+                }
+
+                retorno = Cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                FechaConexao();
+            }
+
+            return retorno;
+        }
+
+        public object ExecuteScalar(string pComandoSQL, List<DBParametros> pParametros = null)
+        {
+            object retorno;
+            AbreConexao();
+            try
+            {
+                Cmd.Connection = Conn;
+                Cmd.Parameters.Clear();
+                Cmd.CommandText = pComandoSQL;
+                Cmd.CommandType = CommandType.Text;
+
+                if (pParametros != null)
+                {
+                    foreach (DBParametros item in pParametros)
+                    {
+                        Cmd.Parameters.AddWithValue(item.Name, item.Value == null ? DBNull.Value : item.Value);
+                    }
+                }
+
+                retorno = Cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                FechaConexao();
+            }
+
+            return retorno;
+        }
+
+        public void IniciarTransacao()
+        {
+            Tra = Conn.BeginTransaction();
+            Cmd.Transaction = Tra;
+        }
+
+        public void ComitarTransacao()
+        {
+            Tra.Commit();
+            Cmd.Transaction = null;
+            Tra = null;
+        }
+
+        public void DesfazerTransacao()
+        {
+            Tra.Rollback();
+            Cmd.Transaction = null;
+            Tra = null;
+        }
+    }
+}
